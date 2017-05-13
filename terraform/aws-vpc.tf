6c6c8f737a64f9faf6dcee40edebef28730d619c
soldierxue <sys@soldierxue.cn>
2017-05-13 20:59:21 +0800

    add ec2 instance creation

--- terraform/aws-vpc.tf
+++ terraform/aws-vpc.tf
@@ -61,6 +61,13 @@ module "aws-vpc" {
   source          = "./region-vpc"
   region          = "${var.region}"
   base_cidr_block = "${var.base_cidr_block}"
+  ec2keyname = "${var.ec2keyname}"
+  mysqlPrefix = "${var.mysqlPrefix}"
+  DnsZoneName ="${var.DnsZoneName}"
+  subnet_private2_cidr = "${var.subnet_private2_cidr}"
+  subnet_private1_cidr = "${var.subnet_private1_cidr}"
+  subnet_pub2_cidr = "${var.subnet_pub2_cidr}"
+  subnet_pub1_cidr = "${var.subnet_pub1_cidr}"
 }
 
 # For AWS Cloud
