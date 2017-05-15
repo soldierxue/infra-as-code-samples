ea215b80c2a5de4c5ff8f19f4f8b15576a8263d4
soldierxue <sys@soldierxue.cn>
2017-05-15 16:46:12 +0800

    refactor

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -5,13 +5,11 @@ provider "aws" {
 
 # models VPC
 
-module "aws-vpc" main-vpc{
+module "aws-vpc" {
   source          = "./infra-vpc"
   region          = "${var.region}"
   base_cidr_block = "${var.base_cidr_block}"
   ec2keyname = "${var.ec2keyname}"
-  mysqlPrefix = "${var.mysqlPrefix}"
-  DnsZoneName ="${var.DnsZoneName}"
   subnet_private2_cidr = "${var.subnet_private2_cidr}"
   subnet_private1_cidr = "${var.subnet_private1_cidr}"
   subnet_pub2_cidr = "${var.subnet_pub2_cidr}"
@@ -22,12 +20,12 @@ module "aws-vpc" main-vpc{
 
 module "demo-php" {
   source          = "./demo-php"
-  vpc_id          = "${module.aws-vpc.main-vpc.id}"
-  public_subnet_id = "${module.aws-vpc.main-vpc.module.public_subnet1.subnet_id}"
-  fronend_web_sgid = "${module.aws-vpc.main-vpc.aws_security_group.frontend.id}"
+  vpc_id          = "${module.aws-vpc.aws_vpc.main.id}"
+  public_subnet_id = "${module.aws-vpc.aws_vpc.main.module.public_subnet1.subnet_id}"
+  fronend_web_sgid = "${module.aws-vpc.aws_vpc.main.aws_security_group.frontend.id}"
   
-  private_subnet_id = "${module.aws-vpc.main-vpc.module.private_subnet1.subnet_id}"
-  database_sgid = "${module.aws-vpc.main-vpc.aws_security_group.database.id}"
+  private_subnet_id = "${module.aws-vpc.aws_vpc.main.module.private_subnet1.subnet_id}"
+  database_sgid = "${module.aws-vpc.aws_vpc.main.aws_security_group.database.id}"
   
   ec2keyname = "${var.ec2keyname}"
 }
