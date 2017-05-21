5d10b4af49e2808176a91a4d014d798f9a014a52
soldierxue <sys@soldierxue.cn>
2017-05-21 22:43:25 +0800

    bjs demo

--- terraform/bjs-infra/infra-nat/variables.tf
+++ terraform/bjs-infra/infra-nat/variables.tf
@@ -3,6 +3,6 @@ data "aws_availability_zones" "all" {
 }
 variable ec2_keyname {}
 variable sg_nat_id {}
-varialbe vpc_id {}
+variable vpc_id {}
 variable private_subnet1_id {}
 variable private_subnet2_id {}
