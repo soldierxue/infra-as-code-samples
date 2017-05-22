452ed86d3e25911401fd9cbbc517e76fac07901b
soldierxue <sys@soldierxue.cn>
2017-05-22 15:24:09 +0800

    NAT HA

--- terraform/bjs-infra/demo-php/dns.tf
+++ terraform/bjs-infra/demo-php/dns.tf
@@ -11,18 +11,3 @@ resource "aws_vpc_dhcp_options_association" "dns_resolver" {
     vpc_id = "${var.vpc_id}"
     dhcp_options_id = "${aws_vpc_dhcp_options.mydhcp.id}"
 }
-
-/* DNS PART ZONE AND RECORDS */
-resource "aws_route53_zone" "main" {
-  name = "${var.DnsZoneName}"
-  vpc_id = "${var.vpc_id}"
-  comment = "Route 53 Private Zone Managed by terraform"
-}
-
-resource "aws_route53_record" "database" {
-   zone_id = "${aws_route53_zone.main.zone_id}"
-   name = "${var.mysqlPrefix}.${var.DnsZoneName}"
-   type = "A"
-   ttl = "300"
-   records = ["${aws_instance.database.private_ip}"]
-}
