aea3c28b2972c50cdc2ff9106f81b373c4212bff
soldierxue <sys@soldierxue.cn>
2017-05-22 14:52:43 +0800

    NAT HA

--- terraform/bjs-infra/infra-nat/variables.tf
+++ terraform/bjs-infra/infra-nat/variables.tf
@@ -16,8 +16,18 @@ variable public_subnets {
 }
 variable instance_profile_name {}
 variable aws_region {}
-variable nat_monitor_num_pings {default=3}
-variable nat_monitor_ping_timeout {default=1}
-variable nat_monitor_wait_between_pings {default=2}
-variable nat_monitor_wait_for_instance_stop {default=60}
-variable nat_monitor_wait_for_instance_start {default=300}
+variable nat_monitor_num_pings {
+   default=3
+}
+variable nat_monitor_ping_timeout {
+   default=1
+}
+variable nat_monitor_wait_between_pings {
+    default=2
+}
+variable nat_monitor_wait_for_instance_stop {
+    default=60
+}
+variable nat_monitor_wait_for_instance_start {
+    default=300
+}
