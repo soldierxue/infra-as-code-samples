613080ae181c2d1c4eb4b195d37eb9174f3028ba
soldierxue <sys@soldierxue.cn>
2017-05-22 15:06:04 +0800

    NAT HA

--- terraform/bjs-infra/infra-nat/variables.tf
+++ terraform/bjs-infra/infra-nat/variables.tf
@@ -4,22 +4,8 @@ data "aws_availability_zones" "all" {
 variable ec2_keyname {}
 variable sg_nat_id {}
 variable vpc_id {}
-variable private_subnet1_id {}
-variable private_subnet2_id {}
-variable private_subnets {
-   default = [
-     "${var.private_subnet1_id}",
-     "${var.private_subnet2_id}"
-   ]
-}
-variable public_subnet1_id {}
-variable public_subnet2_id {}
-variable public_subnets {
-   default =[
-     "${var.public_subnet1_id}",
-     "${var.public_subnet2_id}"
-   ]
-}
+variable private_subnets {}
+variable public_subnets {}
 variable instance_profile_name {}
 variable aws_region {}
 variable nat_monitor_num_pings {
