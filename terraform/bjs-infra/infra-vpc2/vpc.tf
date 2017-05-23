6d606e778bab5edbbf1b89e0d325fda4bfa886d2
soldierxue <sys@soldierxue.cn>
2017-05-23 11:43:56 +0800

    vpc module refactor

--- terraform/bjs-infra/infra-vpc2/vpc.tf
+++ terraform/bjs-infra/infra-vpc2/vpc.tf
@@ -96,5 +96,5 @@ resource "aws_route_table_association" "private" {
   count = "${length(data.aws_availability_zones.all.names)}"
    
   subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${aws_route_table.private.id}"
+  route_table_id = "${element(aws_route_table.private.*.id,count.index)}"
 }
