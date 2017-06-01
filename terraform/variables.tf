1cbefc0680f3be1cf845512b46034085e4801ad4
soldierxue <sys@soldierxue.cn>
2017-06-01 22:36:47 +0800

    Modulization for devops repeatable usage

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  type = "string"
-  description = "the region where to create the VPC networking resources"
-}
-
 variable repo_name {
    default = "repo_cc"
    description = "The name of the code commit repository"
