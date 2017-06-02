721991721e65a138e699a93974dfe0598bfc45ad
soldierxue <sys@soldierxue.cn>
2017-06-03 00:32:59 +0800

    fix nat error

--- terraform/global-vpc/vpc.tf
+++ terraform/global-vpc/vpc.tf
@@ -60,6 +60,7 @@ resource "aws_eip" "nat" {
   vpc      = true
 } 
 resource "aws_nat_gateway" "ngw" {
+  count = "${length(data.aws_availability_zones.all.names)}"
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
   depends_on = ["aws_internet_gateway.main"]
