0e3c6012516f1143520028f7a63b49b5b9af3072
soldierxue <sys@soldierxue.cn>
2017-06-02 15:17:38 +0800

    add global vpc module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,4 +1,13 @@
+
+# module to create code commit service
 module "m_code_commit" {
   source          = "./code-commit"
   name          = "${var.repo_name}"
+}
+
+# module to create code commit service
+module "m_global_vpc" {
+  source          = "./global-vpc"
+  name          = "${var.vpc_name}"
+  environment  = "${var.environment}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,20 @@
+### Basic Variables
+
+variable "environment" {
+    default="test"
+    description = "Environment tag, e.g prod"
+}
+
+### For Code Commit
 variable repo_name {
    default = "repo_cc"
    description = "The name of the code commit repository"
 }
+
+variable vpc_name {
+   default = "vpc-jason"
+   description = "The name of the vpc"
+}
+
+
 
