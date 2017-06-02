75af59ede56fffa8d665d003604538caa4ef84de
soldierxue <sys@soldierxue.cn>
2017-06-02 23:35:39 +0800

    fix sg

--- terraform/security-groups/outputs.tf
+++ terraform/security-groups/outputs.tf
@@ -10,14 +10,3 @@ output "sg_internal_id" {
   value = "${aws_security_group.internal.id}"
 }
 
-output "ecs_instance_profile_name" {
-  value = "${aws_iam_instance_profile.ecs_instance.name}"
-}
-
-output "ecs_service_role_arn" {
-  value = "${aws_iam_role.ecs_service.arn}"
-}
-
-output "ecs_service_autoscale_role_arn" {
-  value = "${aws_iam_role.ecs_service_autoscale.arn}"
-}
