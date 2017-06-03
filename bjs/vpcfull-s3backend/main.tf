a3f4214031b35c9717d7888fde128bc0c4ab8a4a
soldierxue <sys@soldierxue.cn>
2017-06-03 22:37:54 +0800

    add output for remote state

--- bjs/vpcfull-s3backend/main.tf
+++ bjs/vpcfull-s3backend/main.tf
@@ -36,3 +36,41 @@ module "bjs-vpc" {
   subnet_private_cidrs = ["10.0.48.0/20","10.0.112.0/20"]
   subnet_public_cidrs = ["10.0.0.0/20","10.0.16.0/20"]
 }
+
+output stack_name {
+  value = "${module.bjs-vpc.stack_name}"
+}
+output environment {
+  value = "${module.bjs-vpc.environment}"
+}
+output vpc_id {
+  value = "${module.bjs-vpc.vpc_id}"
+}
+
+output subnet_private_ids {
+  value = "${module.bjs-vpc.subnet_private_ids}"
+}
+
+output subnet_public_ids {
+  value = "${module.bjs-vpc.subnet_public_ids}"
+}
+
+output private_route_ids {
+  value = "${module.bjs-vpc.private_route_ids}"
+}
+
+output base_cidr_block {
+  value = "${module.bjs-vpc.base_cidr_block}"
+}
+
+output sg_database_id {
+  value = "$module.bjs-vpc.sg_database_id}"
+}
+
+output sg_frontend_id {
+  value = "${module.bjs-vpc.sg_frontend_id}"
+}
+
+output sg_internal_id {
+  value = "${module.bjs-vpc.sg_internal_id}"
+}
