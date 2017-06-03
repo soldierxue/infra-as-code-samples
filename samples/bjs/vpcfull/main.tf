15a0823bf1dff949fc80f84d7478658aba96dff0
soldierxue <sys@soldierxue.cn>
2017-06-03 14:19:48 +0800

    add keyfile support

--- samples/bjs/vpcfull/main.tf
+++ samples/bjs/vpcfull/main.tf
@@ -12,6 +12,7 @@ module "bjs-vpc" {
   stack_name = "bjsdemo"
   environment = "test"
   ec2keyname = "bjsMyKey"
+  keyfile = "~/bjsMyKey.pem"
   subnet_private_cidrs = ["10.0.48.0/20","10.0.112.0/20"]
   subnet_public_cidrs = ["10.0.0.0/20","10.0.16.0/20"]
 }
