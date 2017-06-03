34a49eb30e977930acb7b1eca4ff3ae438feaa7b
soldierxue <sys@soldierxue.cn>
2017-06-03 12:16:33 +0800

    fix route error

--- terraform/global-vpc/vpc.tf
+++ terraform/global-vpc/vpc.tf
@@ -73,7 +73,7 @@ resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.main.id}"
   route {
         cidr_block =  "0.0.0.0/0"
-        gateway_id = "${element(aws_nat_gateway.ngw.*.id, count.index)}"
+        nat_gateway_id = "${element(aws_nat_gateway.ngw.*.id, count.index)}"
   }
   
   tags {
