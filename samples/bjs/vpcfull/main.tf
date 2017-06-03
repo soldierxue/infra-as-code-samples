2bd5416214a781c6cc072f8bc8046879a6170f5d
soldierxue <sys@soldierxue.cn>
2017-06-03 14:10:21 +0800

    update bjs sample

--- samples/bjs/vpcfull/main.tf
+++ samples/bjs/vpcfull/main.tf
@@ -9,6 +9,8 @@ module "bjs-vpc" {
   source          = "github.com/soldierxue/infra-as-code-samples/terraform/bjs-infra"
   region          = "cn-north-1"
   base_cidr_block = "10.0.0.0/16"
+  stack_name = "bjsdemo"
+  environment = "test"
   ec2keyname = "bjsMyKey"
   subnet_private_cidrs = ["10.0.48.0/20","10.0.112.0/20"]
   subnet_public_cidrs = ["10.0.0.0/20","10.0.16.0/20"]
